3d3e3cbfda9d70bdff7267c2fd85c8be3de0e007
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2022-10-13 22:00:18 +0900

    課題途中経過

--- practice3-2.php
+++ practice3-2.php
@@ -1,8 +1,25 @@
 <?php
-define("Tax",1.1);
-echo Tax;
 
+define("Tax",1.1);
 $products = ["鉛筆" => "100" , "消しゴム" => "150" , "物差し" => "500"];
-echo $products["鉛筆"];
+
+foreach($products as $key => $value) {
+    echo $key."の税込み価格は". $value * Tax ."です";
+    echo '<br>';
+}
+
+?>
+
+<?php
+
+define("Tax",1.1);
+$products = ["鉛筆" => "100" , "消しゴム" => "150" , "物差し" => "500"];
+
+function getTaxPrice(Tax,$key => $value) {
+    echo 
+
+}
+
+
 
 ?>
