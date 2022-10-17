32869223073d03ca899375c8a4b683d98866dba0
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2022-10-17 20:08:46 +0900

    課題提出

--- practice3-2.php
+++ practice3-2.php
@@ -1,7 +1,7 @@
 <?php
 //税率、商品
 define("Tax",1.1);
-$products = ["鉛筆" => "100" , "消しゴム" => "150" , "物差し" => "500"];
+$products = ["鉛筆" => 100 , "消しゴム" => 150 , "物差し" => 500];
 
 
 //foreach
@@ -21,5 +21,23 @@ function tax_inc_price($price,$name) {
 tax_inc_price($products["鉛筆"],"鉛筆");
 tax_inc_price($products["消しゴム"],"消しゴム");
 tax_inc_price($products["物差し"],"物差し");
+echo '<br>';
+?>
+
+<?php
+//以前の問題 商品 個数
+$fruits = ["りんご" => 100,"みかん" => 150,"もも" => 500];
+$quantity = [3,1,6];
+
+function fruits_price($price,$number,$name) {
+    $total = $price * $number;
+    echo $name . "は" . $total . "円です";
+    echo '<br>';
+}
+
+fruits_price($fruits["りんご"],$quantity[0],"りんご");
+fruits_price($fruits["みかん"],$quantity[1],"みかん");
+fruits_price($fruits["もも"],$quantity[2],"もも");
+
 
 ?>
