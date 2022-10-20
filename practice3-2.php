20e204ec1ceeb774ea8c2e22f464d369d3e7aa5e
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2022-10-20 21:52:19 +0900

    課題修正

--- practice3-2.php
+++ practice3-2.php
@@ -6,22 +6,17 @@ $products = ["鉛筆" => 100 , "消しゴム" => 150 , "物差し" => 500];
 
 //foreach
 foreach($products as $key => $value) {
-    echo $key."の税込み価格は". $value * Tax ."円です";
-    echo '<br>';
-}
-echo '<br>';
-
-//関数
-function tax_inc_price($price,$name) {
-    $tax_price = $price * Tax ;
-    echo $name . "の税込み価格は". $tax_price . "円です";
-    echo '<br>';
-}
-
+    function tax_inc_price($price,$name) {
+        $tax_price = $price * Tax ;
+    echo $name."の税込み価格は". $tax_price ."円です";
+    echo '<br>';}
+   
 tax_inc_price($products["鉛筆"],"鉛筆");
 tax_inc_price($products["消しゴム"],"消しゴム");
 tax_inc_price($products["物差し"],"物差し");
 echo '<br>';
+    }
+
 ?>
 
 <?php
