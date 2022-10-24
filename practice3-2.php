4a26cbf64b56cea8d1c2aaac0c1df8f4d8d43b8c
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2022-10-24 21:18:58 +0900

    修正

--- practice3-2.php
+++ practice3-2.php
@@ -3,18 +3,14 @@
 define("Tax",1.1);
 $products = ["鉛筆" => 100 , "消しゴム" => 150 , "物差し" => 500];
 
+function tax_inc_price($price,$name) {
+    $tax_price = $price * Tax ;
+echo $name."の税込み価格は". $tax_price ."円です";
+echo '<br>';}
 
 //foreach
 foreach($products as $key => $value) {
-    function tax_inc_price($price,$name) {
-        $tax_price = $price * Tax ;
-    echo $name."の税込み価格は". $tax_price ."円です";
-    echo '<br>';}
-   
-tax_inc_price($products["鉛筆"],"鉛筆");
-tax_inc_price($products["消しゴム"],"消しゴム");
-tax_inc_price($products["物差し"],"物差し");
-echo '<br>';
+tax_inc_price($products[$key],$key);
 }
 
 ?>
